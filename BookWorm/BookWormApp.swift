a1cf97fe385c9c12939764d29d7f37d5cb3f6e0c
Resn4R <purview-joust.0x@icloud.com>
2023-10-19 17:06:10 +0100

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Vito Borghi on 19/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
