51b7dd7b89573e736e7373e28f760e452d8588f6
Resn4R <purview-joust.0x@icloud.com>
2023-10-19 23:49:36 +0100

    added more stuff, it was all a lie. next commit will be the actual app

--- BookWorm/ContentView.swift
+++ BookWorm/ContentView.swift
@@ -15,7 +15,19 @@ struct ContentView: View {
                 Text(student.name ?? "Unknown")
             }
             
-            
+            Button("Add") {
+                let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+                let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+                
+                let chosenFirstName = firstNames.randomElement()!
+                let chosenLastName = lastNames.randomElement()!
+                
+                let student = Student(context: moc)
+                student.id = UUID()
+                student.name = "\(chosenFirstName) \(chosenLastName)"
+                
+                try? moc.save()
+            }
         }
     }
 }
