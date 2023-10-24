cf22ef81793773751e031fc27a5e699af3aade2e
Resn4R <purview-joust.0x@icloud.com>
2023-10-24 19:49:52 +0100

    added delete option from detailview

--- BookWorm/DetailView.swift
+++ BookWorm/DetailView.swift
@@ -8,7 +8,11 @@
 import SwiftUI
 
 struct DetailView: View {
+    @Environment (\.dismiss) var dismissView
     let book: Book
+    @Environment(\.managedObjectContext) var moc
+    
+    @State private var showDeleteAlert = false
     
     var body: some View {
         ScrollView{
@@ -39,6 +43,26 @@ struct DetailView: View {
         }
         .navigationTitle(book.title ?? "Unknown Book")
         .navigationBarTitleDisplayMode(.inline)
+        .toolbar{
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    showDeleteAlert.toggle()
+                } label: {
+                    Image(systemName: "trash")
+                }
+            }
+        }
+        .alert("Are you sure?", isPresented: $showDeleteAlert) {
+            Button("Delete", role: .destructive) {
+                moc.delete(book)
+                try? moc.save()
+                
+                dismissView()
+            }
+            Button("Cancel", role: .cancel) {}
+        } message: {
+            Text("Do you want to delete this book? This action cannot be undone.")
+        }
         
     }
 }
